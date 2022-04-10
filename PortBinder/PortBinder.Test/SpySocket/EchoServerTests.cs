27bfef8254ba84f08237cf15c45b410c4bc9219d
cola314 <ubj0314@naver.com>
2022-04-10 17:57:42 +0900

    Test: 통합 테스트를 위한 에코 서버 모듈 추가

---
+++ PortBinder/PortBinder.Test/SpySocket/EchoServerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using Xunit;
+
+namespace PortBinder.Test.SpySocket;
+
+public class EchoServerTests : IDisposable
+{
+    private EchoServer server;
+    private int PORT = 1616;
+
+    public EchoServerTests()
+    {
+        server = new EchoServer();
+        _ = server.RunAsync(PORT);
+    }
+
+    public void Dispose()
+    {
+        server.Stop();
+    }
+
+    [Fact]
+    public void SendHelloAndReceiveHello()
+    {
+        using var client = new TcpClient();
+        client.Connect("localhost", PORT);
+
+        using var stream = client.GetStream();
+        using var writer = new StreamWriter(stream) { AutoFlush = true };
+        writer.WriteLine("Hello");
+
+        using var reader = new StreamReader(stream);
+        var result = reader.ReadLine();
+
+        Assert.Equal("Hello", result);
+    }
+}
