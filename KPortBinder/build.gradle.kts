cbc7d8ff753de58b35f3b133d53a172fe9c1e4f7
cola314 <ubj0314@naver.com>
2024-04-15 22:48:20 +0900

    feat: 실행 가능한 jar 파일 생성

--- KPortBinder/build.gradle.kts
+++ KPortBinder/build.gradle.kts
@@ -16,6 +16,15 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.portbinder.MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
 kotlin {
     jvmToolchain(17)
 }
